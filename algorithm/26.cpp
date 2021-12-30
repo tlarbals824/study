d50b84857351de53b678397e063e2032ddac6c6c
- <simkm0824@naver.com>
2021-12-30 08:56:24 +0000

    26. 마라톤

--- algorithm/26.cpp
+++ algorithm/26.cpp
@@ -5,7 +5,20 @@
 using namespace std;
 
 int main(){
-	printf("HEllo");
-	printf("HEllo");
-	printf("HEllo");
+	int n;
+	scanf("%d",&n);
+	int runner[n];
+	int runnerScore[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&runner[i]);
+		runnerScore[i]=i+1;
+	}
+	for(int i=1;i<n;i++){
+		for(int j=0;j<i;j++){
+			if(runner[i]>runner[j]) runnerScore[i]--;
+		}
+	}
+	for(int i=0;i<n;i++){
+		printf("%d ",runnerScore[i]);
+	}
 }
