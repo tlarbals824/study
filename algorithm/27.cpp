ab808cbdf9f37f2cf97cf870c2907630078adfb0
- <simkm0824@naver.com>
2022-01-21 05:03:51 +0000

    27. N!의 표현법

--- algorithm/27.cpp
+++ algorithm/27.cpp
@@ -5,5 +5,26 @@
 using namespace std;
 
 int main(){
-	printf("HElloWOrld!\n");
+	int num;
+	scanf("%d",&num);
+	printf("%d! = ",num);
+	vector<int> ch(num+1); //기본적으로 vector는 0으로 초기화되어있음
+	for(int i=2;i<=num;i++){
+		int tmp=i;
+		int j=2;
+		for(;;){
+			if(tmp%j==0){
+				ch[j]++;
+				tmp=tmp/j;
+			}
+			else{
+				j++;
+			}
+			if(tmp==1) break;
+		}
+	}
+	for(int i=2;i<=num;i++){
+		if(ch[i]!=0) printf("%d ",ch[i]);
+	}
+	printf("\n");
 }
