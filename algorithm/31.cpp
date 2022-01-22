cf5c54d457ce9b8d71ea74a86a3284dcc5b84fe6
- <simkm0824@naver.com>
2022-01-22 06:05:43 +0000

    31. 탄화수소 질량

---
+++ algorithm/31.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+
+int main(){
+	char ch[8];
+	int cCount=0,hCount=0;
+	int sum;
+	int count=1;
+	scanf("%s",ch);
+	while(1){
+		if(ch[count]=='H') {
+			count++;
+			break;
+		}
+		cCount=cCount*10+(ch[count]-48);
+		count++;
+	}
+	if(cCount==0) cCount=1;
+	while(1){
+		if(ch[count]=='\0') break;
+		hCount=hCount*10+(ch[count]-48);
+		count++;
+	}
+	if(hCount==0) hCount=1;
+	sum=cCount*12+hCount;
+	printf("%d\n",sum);
+}
