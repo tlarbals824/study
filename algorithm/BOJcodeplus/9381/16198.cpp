7b911607fbf390c99cd7126447c929202550dd31
b911099 <simkm0824@naver.com>
2022-03-25 03:53:23 +0000

    b911099

--- algorithm/BOJcodeplus/9381/16198.cpp
+++ algorithm/BOJcodeplus/9381/16198.cpp
@@ -1,9 +1,44 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+int n;
+int res=-1;
+int dp[20];
+void dfs(int sum,int cnt){
+	if(cnt==n-2){
+		res=max(res,sum);
+	}else{
+		for(int i=2;i<n;i++){
+			if(dp[i]!=0){
+				int tmp=dp[i];
+				dp[i]=0;
+				int s1,s2;
+				for(int j=i-1;j>0;j--){
+					if(dp[j]!=0){
+						s1=dp[j];
+						break;
+					}
+				}
+				for(int j=i+1;j<=n;j++){
+					if(dp[j]!=0){
+						s2=dp[j];
+						break;
+					}
+				}
+				dfs(sum+s1*s2,cnt+1);
+				dp[i]=tmp;
+			}
+		}
+	}
+}
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	
-	
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>dp[i];
+	}
+	dfs(0,0);
+	cout<<res<<'\n';
 }
--- algorithm/BOJcodeplus/9381/9663.cpp
+++ algorithm/BOJcodeplus/9381/9663.cpp
@@ -1,56 +1,36 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
+int dp[20]={0};
 int n;
 int cnt=0;
-int dp[20][20]={0};
-void dfs(int c,int y,int x){
-	if(c==n){
+void dfs(int idx){
+	if(idx==n+1){
 		cnt++;
 	}else{
-		if(check(y,x)){
-			dp[y][x]=1;
-			if(x+1==n){
-				dfs(c+1,y+1,0);
-			}else{
-				dfs(c+1,y,x+1);
+		for(int i=1;i<=n;i++){
+			bool flag=false;
+			for(int j=1;j<idx;j++){
+				if(dp[j]==i||dp[j]==idx+i-j||j-dp[j]==idx-i){
+					flag=true;
+					break;
+				}
 			}
-			dp[y][x]=0;
-			if(x+1==n){
-				dfs(c,y+1,0);
-			}else{
-				dfs(c,y,x+1);
-			}
-		}else{
-			if(x+1==n){
-				dfs(c,y+1,0);
-			}else{
-				dfs(c,y,x+1);
+			if(flag) continue;
+			else{
+				dp[idx]=i;
+				dfs(idx+1);
+				dp[idx]=0;
 			}
 		}
 	}
 }
-bool check(int y,int x){
-	//가로
-	for(int i=0;i<n;i++){
-		if(dp[y][i]==1){
-			return false;
-		}
-	}
-	//세로
-	for(int i=0;i<n;i++){
-		if(dp[i][x]==1){
-			return false;
-		}
-	}
-	//대각선1
-	for(int )
-	//대각선2
-}
-
 int main(){
 	ios_base::sync_with_stdio(false);
 	
 	cin>>n;
-	dfs(0,0,0);
+	
+	dfs(1);
+	cout<<cnt<<'\n';
 }
